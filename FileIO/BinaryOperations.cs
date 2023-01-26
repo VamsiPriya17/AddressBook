62d124291012dddbd4cab6df870a340a55d20d45
Nulu <223089864@ge.com>
2023-01-26 22:54:57 +0530

    Day 7: File Operations to Read and Write into the file

---
+++ FileIO/BinaryOperations.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace FileIO
+{
+    class BinaryOperations
+    {
+        public static void BinarySerialization()
+        {
+            string path = @"C:\Users\223089864\Desktop\BridgeLabz\FileIO\BinaryFile.txt";
+            PersonData data = new PersonData();
+            data.name = "Priya";
+            data.age = 21;
+            data.phoneNum = 098765456;
+            data.email = "abc@xyz.com";
+            data.city = "chennai";
+            data.state = "Tamil Nadu";
+            data.addr = "Inforead Buildings";
+            FileStream file = File.OpenWrite(path);
+            //FileStream file = new FileStream(@"C:\Users\223089864\Desktop\BridgeLabz\FileIO\BinaryFile.txt", FileMode.Create);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(file, data);
+            file.Close();
+        }
+        public static void BinaryDeserialization()
+        {
+            string path = @"C:\Users\223089864\Desktop\BridgeLabz\FileIO\BinaryFile.txt";
+            FileStream file = File.OpenRead(path);
+             
+            BinaryFormatter formatter = new BinaryFormatter();
+            PersonData person = (PersonData)formatter.Deserialize(file);
+            Console.WriteLine("Person Details: ");
+            Console.WriteLine(person.name);
+            Console.WriteLine(person.age);
+            Console.WriteLine(person.phoneNum);
+            Console.WriteLine(person.email);
+            Console.WriteLine(person.city);
+            Console.WriteLine(person.state);
+            Console.WriteLine(person.addr);
+        }
+        public static void JSONSerialization()
+        {
+            string path = @"C:\Users\223089864\Desktop\BridgeLabz\FileIO\JSONFile.json";
+            PersonData person = new PersonData("Bhargav", 16, 0987678, "abc@xyz.com", "Chennai", "Tamil Nadu", "Greenwich Building");
+            string result = JsonConvert.SerializeObject(person);
+            File.WriteAllText(path, result);
+            
+        }
+        public static void JSONDeserialize()
+        {
+            string path = @"C:\Users\223089864\Desktop\BridgeLabz\FileIO\JSONFile.json";
+            string result = File.ReadAllText(path);
+            PersonData person = JsonConvert.DeserializeObject<PersonData>(result);
+            Console.WriteLine("Person Details: ");
+            Console.WriteLine(person.name);
+            Console.WriteLine(person.age);
+            Console.WriteLine(person.phoneNum);
+            Console.WriteLine(person.email);
+            Console.WriteLine(person.city);
+            Console.WriteLine(person.state);
+            Console.WriteLine(person.addr);
+        }
+    }
+}
---
+++ FileIO/PersonData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO
+{
+    [Serializable]
+    public class PersonData
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public long phoneNum;
+        public string email;
+        public string city;
+        public string state;
+        public string addr;
+        public PersonData(string name, int age, long phoneNum, string email, string city, string state, string addr)
+        {
+            this.name = name;
+            this.age = age;
+            this.phoneNum = phoneNum;
+            this.email = email;
+            this.city = city;
+            this.state = state;
+            this.addr = addr;
+        }
+        public PersonData()
+        {
+
+        }
+    }
+}
